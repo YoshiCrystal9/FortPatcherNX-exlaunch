e6cd08196c6759ec34e939840abb5eb987fa3099
YoshiCrystal9 <89108143+YoshiCrystal9@users.noreply.github.com>
2023-07-02 00:44:38 +0200

    Update main.cpp

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -1,9 +1,13 @@
 #include "lib.hpp"
 #include "logger.cpp"
 
+void main() {
+    Logger::Initialize();
+}
+
 /* Define hook StubCopyright. Trampoline indicates the original function should be kept. */
 /* HOOK_DEFINE_REPLACE can be used if the original function does not need to be kept. */
-HOOK_DEFINE_TRAMPOLINE(StubCopyright) {
+HOOK_DEFINE_TRAMPOLINE(LoggingThing) {
 
     /* Define the callback for when the function is called. Don't forget to make it static and name it Callback. */
     static void Callback(bool enabled) {
@@ -14,11 +18,8 @@ HOOK_DEFINE_TRAMPOLINE(StubCopyright) {
 
 };
 
-
-/* Declare function to dynamic link with. */
-namespace nn::oe {
-    void SetCopyrightVisibility(bool);
-    Logger::Initialize();
+namespace nn::diag::detail {
+    void LogImpl();
 };
 
 extern "C" void exl_main(void* x0, void* x1) {
@@ -26,16 +27,8 @@ extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
 
     /* Install the hook at the provided function pointer. Function type is checked against the callback function. */
-    StubCopyright::InstallAtFuncPtr(nn::oe::SetCopyrightVisibility);
+    LoggingThing::InstallAtFuncPtr(nn::diag::detail::LogImpl);
 
-    /* Alternative install funcs: */
-    /* InstallAtPtr takes an absolute address as a uintptr_t. */
-    /* InstallAtOffset takes an offset into the main module. */
-
-    /*
-    For sysmodules/applets, you have to call the entrypoint when ready
-    exl::hook::CallTargetEntrypoint(x0, x1);
-    */
 }
 
 extern "C" NORETURN void exl_exception_entry() {
